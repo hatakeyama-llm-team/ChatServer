e392fef635511243469edfacadfbea25f6d3fb9a
a100 kh <hatakeyama.k.ac@m.titech.ac.jp>
2024-04-15 13:45:23 +0900

    main

--- README.md
+++ README.md
@@ -4,7 +4,13 @@
 ## Google関連
 - [このあたりの記事を参照](https://note.com/alice37308108/n/n7ef1c0f27ea2#51fb3f7a-b695-416b-9b59-168759ba8a97)
 
+## modules
+~~~
+pip install gspread
+~~~
+
 # 動作
 ~~~
 main.py
 ~~~
+
--- main.py
+++ main.py
@@ -5,38 +5,40 @@ from src.Bot import Bot
 import time
 
 
-model_id="kanhatakeyama/0405_100m_clean_ja"
+model_id = "kanhatakeyama/0405_100m_clean_ja"
 model_id = "mistral-community/Mixtral-8x22B-v0.1"
-#model_id="llm-jp/llm-jp-13b-dpo-lora-hh_rlhf_ja-v1.1"
-peft_path="/home/hatakeyama/python/mixtral/outputs/mixtral_1kdolly_1epoch"
+model_id = "llm-jp/llm-jp-13b-dpo-lora-hh_rlhf_ja-v1.1"
+peft_path = "/home/hatakeyama/python/mixtral/outputs/mixtral_1kdolly_1epoch"
+peft_path = ""
 
 with open("env/url.txt") as f:
     url = f.read().strip()
 
-#クライアント
-client=Client(url)
+# クライアント
+client = Client(url)
 
-#モデル
+# モデル
 
-bot=Bot(model_id,peft_path=peft_path)
+bot = Bot(model_id, peft_path=peft_path)
 
 while True:
-    #未回答の質問を取得
+    # 未回答の質問を取得
     while True:
         try:
-            row_id,question,inst=client.get_unanswered_question()
-            if question=="":
+            row_id, question, inst = client.get_unanswered_question()
+            if question == "":
                 print("no question to answer")
                 break
-                
-            prompt=inst+"\n###入力:\n"+question+"\n###応答:\n"
+
+            prompt = inst+"\n###入力:\n"+question+"\n###応答:\n"
             print(prompt)
 
-            #回答させる
-            response=bot.ask(question)
+            # 回答させる
+            response = bot.ask(question)
 
-            #回答を送信 
-            client.answer(row_id,"model:"+response,model_id+"time:"+datetime.datetime.now().isoformat())
+            # 回答を送信
+            client.answer(row_id, "model:"+response, model_id +
+                          "time:"+datetime.datetime.now().isoformat())
         except Exception as e:
             print(e)
             time.sleep(5)
